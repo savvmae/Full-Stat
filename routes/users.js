95a7ba560a42ec19403be289e49eb4f10f31f8ac
Savannah Cosby <savannah.cosby123@gmail.com>
2017-08-07 14:58:05 -0400

    added hashing, ready for test deploy

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const route = express.Router();
 const bodyParser = require('body-parser')
 const passportJWT = require("passport-jwt");;
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
 const ExtractJwt = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
 const data = require('../models/data');
@@ -16,18 +18,15 @@ jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
 jwtOptions.secretOrKey = 'icanauthenticatestuff';
 
 
-// route.get('*', (request, response) => {
-//   response.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
-
 route.post('/api/signup', async function (request, response) {
     if (request.body.email && request.body.password && request.body.userName) {
+        var hashed = crypto.pbkdf2Sync(request.body.password, 'salt', 10, 512, 'sha512').toString('base64');
         var user = await data.users.find({ email: request.body.email });
         if (!user[0]) {
             var newUser = await data.users.create({
                 userName: request.body.userName,
                 email: request.body.email,
-                password: request.body.password
+                password: hashed
             });
             var payload = { id: newUser._id, name: newUser.userName };
             var token = jwt.sign(payload, jwtOptions.secretOrKey);
@@ -45,12 +44,12 @@ route.post('/api/signup', async function (request, response) {
 route.post('/api/login', async function (request, response) {
     if (request.body.email && request.body.password) {
         var email = request.body.email;
-        var password = request.body.password;
+        var hashed = crypto.pbkdf2Sync(request.body.password, 'salt', 10, 512, 'sha512').toString('base64');
     }
     var user = await data.users.find({ email: email });
     if (!user[0]) {
         return response.status(401).json({ message: "no such user found" });
-    } if (user[0].password === request.body.password) {
+    } if (user[0].password === hashed) {
         var payload = { id: user[0]._id, name: user[0].userName };
         var token = jwt.sign(payload, jwtOptions.secretOrKey);
         return response.json({ message: "ok", token: token });
@@ -58,6 +57,6 @@ route.post('/api/login', async function (request, response) {
         return response.status(401).json({ message: "passwords did not match" });
     }
 });
-// add logout button, destroy session, maybe look up article that walks through jwt and passport
+
 
 module.exports = route;
